ecc4b1c54a09d4a92641e8b52169a8b4c17cbaf0
I K <2401680@cognizant.com>
2025-07-03 12:25:15 +0530

    added testNG dependency , added POM for Login and Registration Pages

--- src/test/java/Test_case01/Test1.java
+++ src/test/java/Test_case01/Test1.java
@@ -3,13 +3,17 @@ package Test_case01;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import pages.LoginPage;
+import pages.RegistrationPage;
+
 public class Test1 {
     public static void main(String[] args) {
         WebDriver driver = new ChromeDriver();
-        String url = "http://localhost:3000/";
+        String url = "http://localhost:3000/login";
         driver.get(url);
-
-        System.out.println(driver.getTitle());
-
+//        RegistrationPage register = new RegistrationPage(driver);
+//        register.register("sample","Mypass@123","sample@email.com","Name A B");
+        LoginPage login = new LoginPage(driver);
+        login.login("sample","Mypass@123");
     }
 }
---
+++ src/test/java/pages/RegistrationPage.java
@@ -0,0 +1,61 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class RegistrationPage {
+	WebDriver driver;
+	
+	@FindBy(name="username")
+    private WebElement usernameElement;
+	
+	@FindBy(name="password")
+    private WebElement passwordElement;
+	
+	@FindBy(name="email")
+    private WebElement emailElement;
+	
+	@FindBy(name="name")
+    private WebElement nameElement;
+	
+	@FindBy(xpath="//*[@id=\"root\"]/div/div/div/div[2]/form/button")
+    private WebElement registerElement;
+	
+	@FindBy(xpath="//*[@id=\"root\"]/div/div/div/div[1]/a/button")
+    private WebElement loginElement;
+	
+	
+	public RegistrationPage(WebDriver driver) {
+		// TODO Auto-generated constructor stub
+		this.driver = driver;
+        PageFactory.initElements(driver, this);
+	}
+	public void setUserName(String userName) {
+		usernameElement.sendKeys(userName);
+	}
+	public void setName(String name) {
+		nameElement.sendKeys(name);
+	}
+	public void setPassword(String password) {
+		passwordElement.sendKeys(password);
+	}
+	public void setEmail(String email) {
+		emailElement.sendKeys(email);
+	}
+		
+	
+	public void register(String userName, String password,String email, String name) {
+		usernameElement.sendKeys(userName);
+		emailElement.sendKeys(email);
+		passwordElement.sendKeys(password);
+		nameElement.sendKeys(name);
+		registerElement.click();
+	}
+	
+	public void goToLoginPage() {
+		loginElement.click();
+	}
+}
+
