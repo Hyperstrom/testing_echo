64e794bd8c28a2b11b22de6a4b9c108cc510ef64
aditraj01 <adit.raj010@gmail.com>
2025-07-07 11:20:11 +0530

    refactor: clean up whitespace and improve formatting in HomePage class

--- src/test/java/pages/HomePage.java
+++ src/test/java/pages/HomePage.java
@@ -1,54 +1,45 @@
 package pages;
 
-import java.time.Duration;
-
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class HomePage {
 
 	WebDriver driver;
-	
+
 	@FindBy(xpath = "//*[@id=\"root\"]/div/div/div[1]/div[1]/a/svg")
 	private WebElement homeElement;
-	
-	@FindBy(xpath="//*[@id=\"root\"]/div/div/div[1]/div[1]/svg")
+
+	@FindBy(xpath = "//*[@id=\"root\"]/div/div/div[1]/div[1]/svg")
 	private WebElement toggleElement;
-	
-	@FindBy(xpath="//*[@id=\"root\"]/div/div/div[1]/div[2]/div/input")
+
+	@FindBy(xpath = "//*[@id=\"root\"]/div/div/div[1]/div[2]/div/input")
 	private WebElement searchElement;
-	
-	@FindBy(xpath="//*[@id=\"root\"]/div/div/div[1]/div[3]/a/svg")
+
+	@FindBy(xpath = "//*[@id=\"root\"]/div/div/div[1]/div[3]/a/svg")
 	private WebElement profileElement;
-	
-	@FindBy(xpath="//*[@id=\"root\"]/div/div/div[2]/div[1]/div/div/div[1]")
+
+	@FindBy(xpath = "//*[@id=\"root\"]/div/div/div[2]/div[1]/div/div/div[1]")
 	private WebElement exploreElement;
-	
-	@FindBy(xpath="//*[@id=\"root\"]/div/div/div[2]/div[1]/div/div/div[2]")
+
+	@FindBy(xpath = "//*[@id=\"root\"]/div/div/div[2]/div[1]/div/div/div[2]")
 	private WebElement notificationsElement;
-	
-	@FindBy(xpath="//*[@id=\"root\"]/div/div/div[2]/div[1]/div/div/div[3]")
+
+	@FindBy(xpath = "//*[@id=\"root\"]/div/div/div[2]/div[1]/div/div/div[3]")
 	private WebElement messagesElement;
-	
-	@FindBy(xpath="//*[@id=\"root\"]/div/div/div[2]/div[1]/div/div/div[4]")
+
+	@FindBy(xpath = "//*[@id=\"root\"]/div/div/div[2]/div[1]/div/div/div[4]")
 	private WebElement friendsElement;
-	
-	@FindBy(xpath="//*[@id=\root\"]/div/div/div[1]/div[3]/div[1]/svg")
+
+	@FindBy(xpath = "//*[@id=\root\"]/div/div/div[1]/div[3]/div[1]/svg")
 	private WebElement navbarMessagesElement;
-	
-	@FindBy(xpath="//*[@id=\"root\"]/div/div/div[1]/div[3]/div[3]")
+
+	@FindBy(xpath = "//*[@id=\"root\"]/div/div/div[1]/div[3]/div[3]")
 	private WebElement logoutElement;
-	
-//	@FindBy(xpath="")
-//	private WebElement
-	
-	
+
 	public HomePage(WebDriver driver) {
-		// TODO Auto-generated constructor stub
 		this.driver = driver;
 		PageFactory.initElements(driver, this);
 	}
