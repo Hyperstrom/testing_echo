4bf5c342438c6e4eb2915046942d78bec28e492e
aditraj01 <adit.raj010@gmail.com>
2025-07-07 17:42:14 +0530

    feat: add UI element locators and visibility checks for RegistrationPage

--- src/test/java/testScenario2/RegistrationTest.java
+++ src/test/java/testScenario2/RegistrationTest.java
@@ -4,7 +4,7 @@ import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertTrue;
 
 import java.util.List;
-import java.util.UUID; // For generating unique IDs
+import java.util.UUID;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -32,7 +32,27 @@ public class RegistrationTest {
 		loginPage.goToRegistrationPage();
 	}
 
-	@Test(description = "Verify successful registration possible using correct credentials")
+	@Test(description = "Verify all the Elements are present", groups = { "ui", "sanity", "smoke" })
+	public void testUIForRegistrationPage() {
+		assertTrue(registerPage.isRegistrationPageDisplayed(), "Registration page itself is not displayed.");
+
+		assertTrue(registerPage.isElementDisplayed(RegistrationPage.USERNAME_INPUT_LOCATOR),
+				"Username input field is not displayed");
+		assertTrue(registerPage.isElementDisplayed(RegistrationPage.EMAIL_INPUT_LOCATOR),
+				"Email input field is not displayed");
+		assertTrue(registerPage.isElementDisplayed(RegistrationPage.PASSWORD_INPUT_LOCATOR),
+				"Password input field is not displayed");
+		assertTrue(registerPage.isElementDisplayed(RegistrationPage.NAME_INPUT_LOCATOR),
+				"Name input field is not displayed");
+
+		assertEquals(registerPage.getElementText(RegistrationPage.REGISTER_BUTTON_LOCATOR), "Register",
+				"Register button text is incorrect or not displayed");
+		assertEquals(registerPage.getElementText(RegistrationPage.LOGIN_BUTTON_LOCATOR), "Login",
+				"Login button text is incorrect or not displayed");
+	}
+
+	@Test(description = "Verify successful registration possible using correct credentials", groups = { "regression",
+			"positive", "smoke" })
 	public void testSuccessfulRegistration() {
 		String uniqueUsername = "user_" + UUID.randomUUID().toString().substring(0, 8);
 		String uniqueEmail = "email_" + UUID.randomUUID().toString().substring(0, 8) + "@example.com";
@@ -43,7 +63,8 @@ public class RegistrationTest {
 				"Registration Failed: Login Page is not displayed after successful registration.");
 	}
 
-	@Test(description = "Verify that error message is displayed when no credentials are passed")
+	@Test(description = "Verify that error message is displayed when no credentials are passed", groups = {
+			"regression", "negative", "smoke" })
 	public void testEmptyCredentials() {
 		registerPage.register("", "", "", "");
 
@@ -54,21 +75,21 @@ public class RegistrationTest {
 		assertEquals(actualErrorText, expectedErrors, "The error messages for empty credentials are not as expected.");
 	}
 
-	@Test(description = "Validate error message when username field is empty.")
+	@Test(description = "Validate error message when username field is empty.", groups = { "regression", "negative" })
 	public void testUserNameFieldIsEmpty() {
 		registerPage.register("", "ad@gmail.com", "Aditya@7157", "Aditya");
 		assertEquals(registerPage.getErrorMessageText(RegistrationPage.USERNAME_ERROR_LOCATOR), "Username is required.",
 				"Error message for empty username is not displayed correctly.");
 	}
 
-	@Test(description = "Validate error message when name field is empty.")
+	@Test(description = "Validate error message when name field is empty.", groups = { "regression", "negative" })
 	public void testNameFieldIsEmpty() {
 		registerPage.register("adi", "ad@gmail.com", "Aditya@7157", "");
 		assertEquals(registerPage.getErrorMessageText(RegistrationPage.NAME_ERROR_LOCATOR), "Name is required.",
 				"Error message for empty name is not displayed correctly.");
 	}
 
-	@Test(description = "Validate error message when password field is empty")
+	@Test(description = "Validate error message when password field is empty", groups = { "regression", "negative" })
 	public void testPasswordFieldIsEmpty() {
 		registerPage.register("adi", "ad@gmail.com", "", "Aditya");
 		assertEquals(registerPage.getErrorMessageText(RegistrationPage.PASSWORD_ERROR_LOCATOR), "Password is required.",
@@ -82,14 +103,14 @@ public class RegistrationTest {
 				"Error message for empty email is not displayed correctly.");
 	}
 
-	@Test(description = "Validate error message when email field is invalid")
+	@Test(description = "Validate error message when email field is invalid", groups = { "regression", "negative" })
 	public void testValidEmailField() {
 		registerPage.register("adi", "adi@.com", "Aditya@7157", "Aditya");
 		assertEquals(registerPage.getErrorMessageText(RegistrationPage.EMAIL_ERROR_LOCATOR),
 				"Email address is invalid.", "Error message for invalid email format is not displayed correctly.");
 	}
 
-	@Test(description = "Validate error message when existing username.")
+	@Test(description = "Validate error message when existing username.", groups = { "regression", "negative" })
 	public void testExistingUsername() {
 		registerPage.register("aditya10", "unique_" + UUID.randomUUID().toString().substring(0, 8) + "@example.com",
 				"Aditya@7157", "Aditya");
@@ -97,7 +118,7 @@ public class RegistrationTest {
 				"This username already exists.", "Error message for existing username is not displayed correctly.");
 	}
 
-	@Test(description = "Validate error message when existing email.")
+	@Test(description = "Validate error message when existing email.", groups = { "regression", "negative" })
 	public void testExistingEmail() {
 		registerPage.register("unique_" + UUID.randomUUID().toString().substring(0, 8), "adi@gmail.com", "Aditya@7157",
 				"Aditya");
@@ -105,7 +126,8 @@ public class RegistrationTest {
 				"This email already exists.", "Error message for existing email is not displayed correctly.");
 	}
 
-	@Test(description = "Validate password is accepted if Password contains underscore \"_\"")
+	@Test(description = "Validate password is accepted if Password contains underscore \"_\"", groups = { "regression",
+			"positive" })
 	public void testGivingUnderScoreAsPassword() {
 		String uniqueUsername = "user_" + UUID.randomUUID().toString().substring(0, 8);
 		String uniqueEmail = "email_" + UUID.randomUUID().toString().substring(0, 8) + "@example.com";
@@ -115,7 +137,7 @@ public class RegistrationTest {
 				"Registration Failed: Underscore ('_') should be accepted as a special character in password.");
 	}
 
-	@Test(description = "Validate error message when name field is numeric only")
+	@Test(description = "Validate error message when name field is numeric only", groups = { "regression", "negative" })
 	public void testGivingNumericValueAsName() {
 		registerPage.register("adit01", "adet@gmail.com", "Abcd@123", "12345678");
 		assertEquals(registerPage.getErrorMessageText(RegistrationPage.NAME_ERROR_LOCATOR),
